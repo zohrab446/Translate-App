377eedd4ca1ac7126c0abad6cb9fad4f4a7013ab
Zohrab <116635367+zohrab446@users.noreply.github.com>
2025-02-04 15:25:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const fromLang = document.querySelector("#from-lang");
 const toLang = document.querySelector("#to-lang");
+const btnTranslate = document.querySelector("#btnTranslate");
+const fromText = document.querySelector("#from-text");
+const toText = document.querySelector("#to-text");
 
 for(let lang in languages) {
     let option = `<option value="${lang}">${languages[lang]}</option>`;
@@ -9,3 +12,16 @@ for(let lang in languages) {
     fromLang.value = "tr-TR";
     toLang.value = "en-GB";
 }
+
+btnTranslate.addEventListener("click", () => {
+    let text = fromText.value;
+    let from = fromLang.value;
+    let to = toLang.value;
+    const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=${from}|${to}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            toText.value = data.responseData.translatedText;
+        });
+});
